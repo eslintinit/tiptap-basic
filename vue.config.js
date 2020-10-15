d85aba8c0a8b40cf67757a6e73e2d64b29c1238c
ignatif <ignatif@gmail.com>
2020-10-15 14:11:05 +0300

    LimitChunkCountPlugin

---
+++ vue.config.js
@@ -0,0 +1,13 @@
+const webpack = require("webpack");
+module.exports = {
+  configureWebpack: {
+    plugins: [
+      new webpack.optimize.LimitChunkCountPlugin({
+        maxChunks: 1
+      })
+    ]
+  },
+  chainWebpack: config => {
+    config.optimization.delete("splitChunks");
+  }
+};
